bfc3227cff954df12b32064d75ef009848058e86
Olena Kurilets <elenakurilets@gmail.com>
2022-12-29 12:37:45 +0100

    Add computer move and add Player move

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-let computerMove = "kamień";
-let playerMove = "papier";
+const rock="ROCK";
+const paper ="PAPER";
+const scissors ="SCISSORS";
+const draw = "DRAW";
 
-printMessage(
-  "Zagrałem " +
-    computerMove +
-    "! Jeśli Twój ruch to" +
-    computerMove +
-    " , to wygrywasz!"
-);
-let randomFraction = Math.random();
+//Computer//
 
-printMessage("Wylosowany ułamek to: " + randomFraction);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let calculation = randomFraction * 3 + 1;
+console.log('the number drawn is: ' + randomNumber); //random number works
 
-printMessage("Ułamek pomnożony przez 3 i powiększony o 1: " + calculation);
+let computerMove = randomNumber;
 
-let roundNumber = Math.floor(calculation);
+if(randomNumber === 1){
+  computerMove = 'stone'
+}else if (randomNumber===2){
+  computerMove='paper'
+}else if (randomNumber===3){
+  computerMove='scissors'
+}else{
+  alert('Choose a number!');
+}
+printMessage('My move is: ' + computerMove);
+console.log(computerMove);
 
-printMessage("Liczba po zaokrągleniu w dół to: " + roundNumber);
+//Player//
+
+let playerInput = prompt('Choose your move: 1 - rock, 2 - paper, 3- scissors.');
+
+console.log('Player has written: ' + playerInput);
+
+let playerMove = playerInput;
+console.log('Player chose'+ playerMove);
+if (playerInput === 1){
+  playerMove = 'stone';
+}else if (playerInput===2){
+  playerMove='paper'
+}else if (playerInput===3){
+  playerMove='scissors'
+}
+
+printMessage('Your move is: ' + playerMove);
