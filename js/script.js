ff6069808cc8d3129a7e3a3505134ec8203d8f79
Olena Kurilets <elenakurilets@gmail.com>
2022-12-29 15:51:23 +0100

    Add Winner script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const rock="ROCK";
-const paper ="PAPER";
-const scissors ="SCISSORS";
-const draw = "DRAW";
+const stone="stone";
+const paper ="paper";
+const scissors ="scissors";
+const draw = "draw";
 
 //Computer//
 
@@ -12,31 +12,46 @@ console.log('the number drawn is: ' + randomNumber); //random number works
 let computerMove = randomNumber;
 
 if(randomNumber === 1){
-  computerMove = 'stone'
+  computerMove = stone;
 }else if (randomNumber===2){
-  computerMove='paper'
+  computerMove=paper;
 }else if (randomNumber===3){
-  computerMove='scissors'
+  computerMove=scissors;
 }else{
   alert('Choose a number!');
 }
-printMessage('My move is: ' + computerMove);
+printMessage('Computer move is: ' + computerMove);
 console.log(computerMove);
 
 //Player//
 
-let playerInput = prompt('Choose your move: 1 - rock, 2 - paper, 3- scissors.');
+let playerInput = prompt('Choose your move: stone,  paper,  scissors.');
 
 console.log('Player has written: ' + playerInput);
 
 let playerMove = playerInput;
+
 console.log('Player chose'+ playerMove);
+
 if (playerInput === 1){
-  playerMove = 'stone';
+  playerMove=stone; 
 }else if (playerInput===2){
-  playerMove='paper'
+  playerMove=paper;
 }else if (playerInput===3){
-  playerMove='scissors'
+  playerMove=scissors;
 }
 
 printMessage('Your move is: ' + playerMove);
+
+//winner
+
+if (computerMove===playerMove){
+  printMessage('Both have won - Draw !');
+} else if (computerMove===stone && playerMove===paper || computerMove===paper && playerMove===scissors || computerMove===scissors && playerMove===stone)
+
+  printMessage('You have won!');
+  else {
+  printMessage('Computer has won!');
+}
+ 
+
