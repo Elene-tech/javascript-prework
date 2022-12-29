5ec68c9e48e49e64ea37eb3c57f6781b363d9bee
Olena Kurilets <elenakurilets@gmail.com>
2022-12-29 23:48:05 +0100

    Add function

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
-const stone="stone";
+/*const stone="stone";
 const paper ="paper";
-const scissors ="scissors";
+const scissors ="scissors";*/
 const draw = "draw";
 
 //Computer//
+//function//
+
+function getMoveName(argMoveId){
+  if(argMoveId===1){
+    return 'stone';
+  } else if(argMoveId===2){
+    return 'paper';
+  } else if (argMoveId===3){
+    return 'scissors';
+  }else{
+    return 'unknown choice';
+  }
+}
+
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('the number drawn is: ' + randomNumber); //random number works
 
-let computerMove = randomNumber;
-
-if(randomNumber === 1){
-  computerMove = stone;
-}else if (randomNumber===2){
-  computerMove=paper;
-}else if (randomNumber===3){
-  computerMove=scissors;
-}else{
-  alert('Choose a number!');
-}
+let computerMove = getMoveName (randomNumber);
 printMessage('Computer move is: ' + computerMove);
-console.log(computerMove);
+
 
 //Player//
 
-let playerInput = prompt('Choose your move: stone,  paper,  scissors.');
+let playerInput = prompt('Choose your move! 1: stone, 2: paper, 3: scissors.');
 
-console.log('Player has written: ' + playerInput);
-
-let playerMove = playerInput;
-
-console.log('Player chose'+ playerMove);
-
-if (playerInput === 1){
-  playerMove=stone; 
-}else if (playerInput===2){
-  playerMove=paper;
-}else if (playerInput===3){
-  playerMove=scissors;
-}
-
+console.log (playerInput);
+let playerMove = getMoveName (playerInput);
 printMessage('Your move is: ' + playerMove);
+
+/*if (playerInput === 1){
+  playerMove='stone'; 
+}else if (playerInput===2){
+  playerMove='paper';
+}else if (playerInput===3){
+  playerMove='scissors';
+}
+*/
+
 
 //winner
 
